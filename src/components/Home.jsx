24ac5c111448e7835076d877b145e5d78f338ca9
joshuaharris1103 <joshuaharris1103@gmail.com>
2023-08-11 21:30:08 -0400

    finished nav bar and interaction. Starting Home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons'
+const Home = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,46 @@
-import React from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
-import Joshua from '../files/joshua.jpg'
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Joshua from "../files/joshua.jpg";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-        <div>
-        <img src={Joshua} alt='my face' style={{width: '50px'}}/>
-        </div>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Joshua} alt="my face" style={{ width: "50px" }} />
+      </div>
 
-        {/* menu */}
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-        {/* Hamburger */}
-        {/* Mobile Menu */}
-        {/* Socials */}
+      {/* menu */}
+      <div className="hidden md:flex">
+        <ul className="md:flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile Menu */}
+      
+        <ul className= {!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center itmes-center'}>
+          <li className="ey-6 text-4xl">Home</li>
+          <li className="ey-6 text-4xl">About</li>
+          <li className="ey-6 text-4xl">Skills</li>
+          <li className="ey-6 text-4xl">Projects</li>
+          <li className="ey-6 text-4xl">Contact</li>
+        </ul>
+
+      {/* Socials */}
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
