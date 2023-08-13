5404b3c00f96667bebcdc9828e56c333da026986
joshuaharris1103 <joshuaharris1103@gmail.com>
2023-08-13 18:02:52 -0400

    technologies used added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import HTML from '../files/html.png'
-import CSS from '../files/html.png'
+import CSS from '../files/css.png'
 import JavaScript from '../files/javascript.png'
 import ReactImg from '../files/react.png'
 import Node from '../files/node.png'
@@ -14,7 +14,7 @@ const Skills = () => {
   return (
     <div name='skills' className='bg-[#0a192f] text-gray-300'>
         {/* Container */}
-    <div className='max-w-[100px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+    <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
             <p className='py-4'>// These are the technologies I have worked with</p>
@@ -24,26 +24,40 @@ const Skills = () => {
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                 <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
                 <p className='my-4'>HTML</p>
-                <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
-                <p className='my-4'>HTML</p>
-                <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
-                <p className='my-4'>HTML</p>
-                <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
-                <p className='my-4'>HTML</p>
-                <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
-                <p className='my-4'>HTML</p>
-                <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
-                <p className='my-4'>HTML</p>
-                <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
-                <p className='my-4'>HTML</p>
-                <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
-                <p className='my-4'>HTML</p>
-                <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
-                <p className='my-4'>HTML</p>
-                <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
-                <p className='my-4'>HTML</p>
             </div>
-        </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={CSS} alt='HTML icon' />
+                <p className='my-4'>CSS</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={JavaScript} alt='HTML icon' />
+                <p className='my-4'>JavaScript</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={ReactImg} alt='HTML icon' />
+                <p className='my-4'>React</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Node} alt='HTML icon' />
+                <p className='my-4'>Node</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={AWS} alt='HTML icon' />
+                <p className='my-4'>AWS</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={GitHub} alt='HTML icon' />
+                <p className='my-4'>GitHub</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Tailwind} alt='HTML icon' />
+                <p className='my-4'>Tailwind</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Mongo} alt='HTML icon' />
+                <p className='my-4'>Mongo</p>
+            </div>
+        </div>    
     </div>
     </div>
   )
