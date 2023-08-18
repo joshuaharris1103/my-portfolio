327bafd175f736e2e0ce38fbc3fbcf66ac50ca4e
joshuaharris1103 <joshuaharris1103@gmail.com>
2023-08-17 22:40:54 -0400

    added links to side navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,22 +89,22 @@ const Navbar = () => {
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
-            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/joshuareginaldharris/" target="_blank">
               LinkedIn <FaLinkedin size={30}/>
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
-            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/joshuaharris1103" target="_blank">
               Github <FaGithub size={30}/>
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
-            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+            <a className='flex justify-between items-center w-full text-gray-300' href="mailto: joshuarh.dev@gmail.com">
               Email <HiOutlineMail size={30}/>
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
-            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://docs.google.com/document/d/1y5WxyzuLuAfRz-Xa7y5xLBlUAhCfze7U/edit?usp=sharing&ouid=112666961400120615124&rtpof=true&sd=true" target="_blank">
               Resume <BsFillPersonLinesFill size={30}/>
             </a>
           </li>
